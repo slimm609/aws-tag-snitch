57965dcba8669c21d1fbdc395c821660bc84ffa5
Brian Davis <brian.davis@ciphertrace.com>
2020-05-10 20:03:05 -0400

    add kill switch

--- ec2_tagger.py
+++ ec2_tagger.py
@@ -11,7 +11,7 @@ def tag_instance(instance):
 
     # Tag the EBS Volumes
     for vol in instance.volumes.all():
-        #print('Updating tags for {} from {}'.format(vol.id, instance))
+        print('Updating tags for {} from {}'.format(vol.id, instance))
         try:
             vol.create_tags(Tags=tags)
         except:
@@ -19,7 +19,7 @@ def tag_instance(instance):
 
         # Tag the Elastic Network Interfaces
     for eni in instance.network_interfaces:
-        #print('Updating tags for {} from {}'.format(eni.id, instance))
+        print('Updating tags for {} from {}'.format(eni.id, instance))
         try:
             eni.create_tags(Tags=tags)
         except:
@@ -30,7 +30,7 @@ def ec2(region):
     print('Processing EC2 Instances')
 
     instances = boto3.resource('ec2', region_name=region).instances.all()
-    #print('ec2 - {}'.format(region))
+    print('ec2 - {}'.format(region))
     with PoolExecutor(max_workers=4) as executor:
         for _ in executor.map(tag_instance, instances):
             pass
@@ -81,7 +81,6 @@ def _get_elb_tags(name='elb',region='us-west-2'):
 
     tags = {}
     client = boto3.client(name, region_name=region)
-    #print('{} - {}'.format(name,region))
     paginator = client.get_paginator('describe_load_balancers')
     for page in paginator.paginate():
         for lb in page[page_name]:
@@ -98,7 +97,7 @@ def _get_elb_tags(name='elb',region='us-west-2'):
 
 def _network_interfaces(filter=None,region='us-west-2'):
     client = boto3.client('ec2',region_name=region)
-    #print('ec2_network - {}'.format(region))
+    print('ec2_network - {}'.format(region))
     paginator = client.get_paginator('describe_network_interfaces')
     for page in paginator.paginate():
         for interface in page['NetworkInterfaces']:
@@ -108,7 +107,7 @@ def _network_interfaces(filter=None,region='us-west-2'):
 
 
 def _tag_network_interface(eni_id, tags,region='us-west-2'):
-    #print('Updating tags for {}'.format(eni_id))
+    print('Updating tags for {}'.format(eni_id))
     ec2 = boto3.resource('ec2',region_name=region)
     eni = ec2.NetworkInterface(eni_id)
     eni.create_tags(Tags=tags)
---
+++ region_kill_switch.py
@@ -0,0 +1,48 @@
+import boto3
+import sys
+from random import randint
+
+def vpc(region='us-west-2'):
+    print('Processing VPCs')
+    client = boto3.client('ec2',region_name=region)
+    ec2 = boto3.resource('ec2',region_name=region)
+    vpcs = client.describe_vpcs()
+    for vpc in vpcs['Vpcs']:
+        ID = vpc['VpcId']
+        nacl_filter = [{'Name':'vpc-id', 'Values': [ID] }]
+        network_acls = client.describe_network_acls(Filters=nacl_filter)
+        nacls = network_acls['NetworkAcls']
+        if nacls:
+            for nacl in nacls:
+                print('{}'.format(nacl['NetworkAclId']))
+                # Block all Inbound traffic
+                client.create_network_acl_entry(
+                    DryRun=True,
+                    CidrBlock='0.0.0.0/0',
+                    Egress=False,
+                    Protocol='-1',
+                    RuleAction='deny',
+                    RuleNumber=1,
+                    NetworkAclId=nacl['NetworkAclId'] 
+                )
+                # Block all Outbound traffic
+                client.create_network_acl_entry(
+                    DryRun=True,
+                    CidrBlock='0.0.0.0/0',
+                    Egress=True,
+                    Protocol='-1',
+                    RuleAction='deny',
+                    RuleNumber=1,
+                    NetworkAclId=nacl['NetworkAclId'] 
+                )
+
+
+#make them verify a random number because it will block traffic for all VPCs in that region.
+rannum=randint(1000, 9999)
+print("Please enter the following number to continue {}: ".format(rannum))
+data = input()
+if int(data) != rannum:
+    print('Error: verification number does not match')
+    sys.exit(1)
+
+vpc('us-west-2')
